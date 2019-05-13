32de121c0bc2b94456d9027b9ab9011e98c16bf7
benk1 <kakengi_bernard@hotmail.com>
2019-05-13 14:01:47 +0300

    fix margins

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,12 @@
 import React from "react";
 
-const Header = (props) => {
-    
-    return (
+const Header = props => {
+  return (
     <div className="header">
-        <h1>World countries List</h1>
-        <h4>Total Number of countries is: {props.countries.length}</h4>
-        <h4>Please Search by Clicking Start with or include button</h4>
-        
+      <h1>World countries List</h1>
+      <h4>Total Number of countries is: {props.countries.length}</h4>
+      <h4>Please Search by Clicking StartWith or Include button</h4>
     </div>
-
-    );
-
-}
+  );
+};
 export default Header;
